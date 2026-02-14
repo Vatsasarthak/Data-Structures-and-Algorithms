a357fe1a32ed8759895d23febbd9d1410b23c51b
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-02-15 01:33:50 +0530

    Implement reverse function for integers

---
+++ ReverseNumber.java
@@ -0,0 +1,10 @@
+class ReverseNumber {
+    public static int reverse(int n) {
+        int rev = 0;
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n = n / 10;
+        }
+        return rev;
+    }
+}
