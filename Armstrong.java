7d720b7a7a10f3cd7be257be21a2b595ce563492
Vatsasarthak <vatsaroysarthak0007@gmail.com>
2026-02-17 18:25:02 +0530

    Add Armstrong number solution

--- Armstrong.java
+++ Armstrong.java
@@ -1,2 +1,17 @@
-public class Armstrong {
+class Armstrong {
+    public boolean Armstrong(int n) {
+        int original = n;
+
+        // count digits
+        int digits = String.valueOf(n).length();
+
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += Math.pow(digit, digits);
+            n /= 10;
+        }
+
+        return sum == original;
+    }
 }
