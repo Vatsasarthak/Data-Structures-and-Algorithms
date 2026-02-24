795c70dd3d63360b4ed09fad9d01de4a6d0e85a0
Vatsasarthak <vatsaroysarthak0007@gmail.com>
2026-02-24 21:01:32 +0530

    Sum of Root to leaf

---
+++ sumRootToLeaf.java
@@ -0,0 +1,62 @@
+public class Main {
+
+    // Definition for binary tree node
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Solution {
+        public int sumRootToLeaf(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode node, int current) {
+            if (node == null) return 0;
+
+            current = current * 2 + node.val;
+
+            if (node.left == null && node.right == null) {
+                return current;
+            }
+
+            return dfs(node.left, current) + dfs(node.right, current);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        /*
+              Example Tree:
+                   1
+                  / \
+                 0   1
+                / \   \
+               0   1   1
+
+           Paths:
+           100 = 4
+           101 = 5
+           111 = 7
+
+           Output: 16
+        */
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(1);
+        root.right.right = new TreeNode(1);
+
+        Solution sol = new Solution();
+        int result = sol.sumRootToLeaf(root);
+
+        System.out.println("Sum of Root to Leaf Binary Numbers: " + result);
+    }
+}
