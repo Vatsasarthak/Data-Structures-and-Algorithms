dc756697bfc9f2dc2b0002de72789cdaca412557
Vatsasarthak <vatsaroysarthak0007@gmail.com>
2026-02-28 03:22:18 +0530

    SortColours

---
+++ SortColours.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class SortColours {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+        while(mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+}
