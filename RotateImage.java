ff24b77956d13869b0a489729ce0356edc8b4370
Vatsasarthak <vatsaroysarthak0007@gmail.com>
2026-03-07 20:17:41 +0530

    Spiral

--- RotateImage.java
+++ RotateImage.java
@@ -1,25 +1,17 @@
 import java.util.*;
-
 public class RotateImage {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter the Row-");
         int n = sc.nextInt();
-
         System.out.println("Enter the Column-");
         int m = sc.nextInt();
-
         int[][] arr = new int[n][m];
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-
         int[][] tras = new int[m][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -27,23 +19,17 @@ public class RotateImage {
             }
         }
         for(int i = 0; i < m; i++){
-
             int left = 0;
             int right = n - 1;
-
             while(left < right){
-
                 int temp = tras[i][left];
                 tras[i][left] = tras[i][right];
                 tras[i][right] = temp;
-
                 left++;
                 right--;
             }
         }
-
         System.out.println("Rotated Matrix:");
-
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 System.out.print(tras[i][j] + " ");
