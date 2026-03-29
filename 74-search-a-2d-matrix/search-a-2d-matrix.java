52f2450f18490f9cce4aac894ac50f5a5dc8b3fa
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-03-30 04:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (72.11%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         for(int i=0;i <matrix.length;i++){
+            if(matrix[i][0] <= target && target <= matrix[i][matrix[i].length -1]){
           if(binarysearch( matrix[i] , target)){
             return true;
           }
           }
+        }
             return false;
     }
     boolean binarysearch(int[] row ,int target){
