ab1ffad0b5cc8f438e51e04d26776b3ae97c6b6e
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-04-16 18:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (93.74%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Map<Character, Character> mapping = new HashMap<>();
-        mapping.put(')', '(');
-        mapping.put('}', '{');
-        mapping.put(']', '[');
-
-        for (char c : s.toCharArray()) {
-            if (mapping.containsValue(c)) {
-                stack.push(c);
-            } else if (mapping.containsKey(c)) {
-                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
-                    return false;
-                }
-            }
-        }
-
-        return stack.isEmpty();        
+        if(s.length() % 2 == 1)
+            return false;
+        return isValid(0, ' ', s.toCharArray()) >= 0;
     }
+
+    private int isValid(int i,char last, char[] s){
+        if(i == s.length){
+            return i;
+        }
+        if(i > s.length){
+            return -1;
+        }
+        if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+            int next = isValid(i + 1, s[i], s);
+            return next < 0 ? -1 : isValid(next + 1, last, s);
+        }
+        if(s[i] == ')' && last == '(' || s[i] == ']' && last == '[' ||s[i] == '}' && last == '{'){
+            return i;
+        }
+        return -1;
+    }
+    
 }
