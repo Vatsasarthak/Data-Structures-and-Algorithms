b931580a2201aaac06dd8ffb3563907ed5c180b9
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-04-18 16:41:00 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (99.89%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,17 +1,44 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        String[] array =  new String[nums.length];
-        for(int i=0; i<nums.length; i++){
-            array[i] = String.valueOf(nums[i]);
+        quickSort(nums, 0, nums.length - 1);
+        if(nums[0] == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        for(int num : nums) sb.append(num);
+        return sb.toString();
+    }
+    private void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    private void quickSort(int[] nums, int start, int end){
+        if(start >= end) return;
+        int par = nums[end];
+        int l = start, r = end - 1;
+        while(l <= r){
+            while(l <= r && compare(nums[l], par)){
+                l++;
+            }
+            while(l <= r && compare(par, nums[r])){
+                r--;
+            }
+            if(l < r) swap(nums, l, r);
         }
-        Arrays.sort(array,(a,b)-> (b+a).compareTo(a+b));
-        if(array[0].equals("0")){
-            return "0";
+        swap(nums, l, end);
+        quickSort(nums, start, l - 1);
+        quickSort(nums, l + 1, end);
+    }
+    private boolean compare(int a, int b){
+        long aBase = calcBase(a), bBase = calcBase(b);
+        return (a * bBase + b) >= (b * aBase + a);       
+    }
+    private long calcBase(int num){
+        if(num == 0) return 10;
+        long ans = 1;
+        while(num > 0) {
+            ans = ans * 10;
+            num /= 10;
         }
-        StringBuilder largest = new StringBuilder();
-        for(int i=0; i<array.length; i++){
-            largest.append(array[i]);
-        }
-        return largest.toString();
+        return ans;
     }
 }
