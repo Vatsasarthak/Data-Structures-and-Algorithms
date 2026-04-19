b7f1aac00b7acff295a6d5b3a2ba735147cf8880
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-04-19 12:50:50 +0530

    Time: 1481 ms (3.69%) | Memory: 134.7 MB (5.60%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -1,22 +1,34 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int n=nums.length;
-        int minR[]=new int[n];
-
-        int curr=(int)1e9;
-        for(int i=n-1;i>=0;i--){
-            curr=Math.min(curr,nums[i]);
-            minR[i]=curr;
+        int ans = Integer.MAX_VALUE;
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n= nums.length;
+        for(int i = 0 ; i < n ; i++ ){
+            while(!dq.isEmpty() && nums[dq.peekLast()] >= nums[i] ){
+                dq.pollLast();
+            }
+            dq.addLast(i);
+            // System.out.println(dq.peekFirst());
         }
-
-        int maxi=-(int)1e9;
-        for(int i=0;i<n;i++){
-             maxi=Math.max(maxi,nums[i]);
-            int mini=minR[i];
-            if(maxi-mini <= k) return i;
+        // System.out.println(dq.size());
+        int max = nums[0];
+        for(int i = 0  ; i < n ; i++){
+            max = Math.max(max , nums[i]);
+            // System.out.println("Max =" + max);
+            while(!dq.isEmpty() && dq.peekFirst() < i ){
+                dq.pollFirst();
+            }
+            // System.out.println("Min =" +nums[dq.peekLast()]);
+            if(!dq.isEmpty()){
+                int min = nums[dq.peekFirst()];
+                int st = max - min;
+                System.out.println("St =" + st);
+                if(st <= k){
+                    return i;
+                }
+            }
             
         }
-
         return -1;
     }
 }
