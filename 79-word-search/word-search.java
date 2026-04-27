3c047dafb2238eb8f487d814ebfb708517a4f019
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-04-27 18:33:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (31.21%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,42 +1,65 @@
-public class Solution {
+class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
-                }
+        int m = board.length, n = board[0].length;
+        if (m*n < word.length())
+            return false;
+        char[] wrd = word.toCharArray();
+        int[] boardf = new int[128];
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                ++boardf[board[i][j]];
             }
         }
-        
+        for (char ch : wrd)
+        {
+            if (--boardf[ch] < 0)
+            {
+                return false;
+            }
+        }
+        if (boardf[wrd[0]] > boardf[wrd[wrd.length - 1]])
+            reverse(wrd);
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (wrd[0] == board[i][j]
+                    && found(board, i, j, wrd, new boolean[m][n], 0))
+                    return true;
+            }
+        }
         return false;
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
-            return true;
+
+    private void reverse(char[] word)
+    {
+        int n = word.length;
+        for (int i = 0; i < n/2; ++i)
+        {
+            char temp = word[i];
+            word[i] = word[n - i - 1];
+            word[n - i - 1] = temp;
         }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+    }
+    private static final int[] dirs = {0, -1, 0, 1, 0};
+    private boolean found(char[][] board, int row, int col, char[] word,
+                        boolean[][] visited, int index)
+    {
+        if (index == word.length)
+            return true;
+        if (row < 0 || col < 0 || row == board.length || col == board[0].length
+            || board[row][col] != word[index] || visited[row][col])
             return false;
+        visited[row][col] = true;
+        for (int i = 0; i < 4; ++i)
+        {
+            if (found(board, row + dirs[i], col + dirs[i + 1],
+                word, visited, index + 1))
+                return true;
         }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
+        visited[row][col] = false;
         return false;
     }
 }
