dd724f79a862adcbd70a5cf8eb09a9556af96ffc
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-04-30 18:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (55.32%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -2,46 +2,46 @@ class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
 
         ListNode temp = head;
-        ListNode dummy = new ListNode(); 
+        ListNode dummy = new ListNode();
         ListNode ans = dummy;
 
         int count = 0;
         ListNode currHead = head;
 
-        while (temp!= null){
+        while (temp != null) {
 
-            count++; 
+            count++;
 
-            if(count == k){
-                ListNode nextNode = temp.next; 
-                temp.next = null; 
+            if (count == k) {
+                ListNode nextNode = temp.next;
+                temp.next = null;
 
-                ListNode reversedLL = reverse(currHead); 
-                ans.next = reversedLL; 
+                ListNode reversedLL = reverse(currHead);
+                ans.next = reversedLL;
 
-                ListNode tailNode = currHead; 
+                ListNode tailNode = currHead;
 
-                tailNode.next = nextNode; 
+                tailNode.next = nextNode;
 
-                ans = tailNode; 
-                temp = nextNode; 
+                ans = tailNode;
+                temp = nextNode;
 
-                currHead = temp; 
-                count=0; 
+                currHead = temp;
+                count = 0;
 
-            }else {
-                temp = temp.next; 
+            } else {
+                temp = temp.next;
             }
         }
-        return dummy.next; 
+        return dummy.next;
     }
 
-    public ListNode reverse(ListNode head){ 
+    public ListNode reverse(ListNode head) {
 
         ListNode current = head;
         ListNode prev = null;
 
-        while(current !=null) {
+        while (current != null) {
             ListNode newNode = current.next;
             current.next = prev;
             prev = current;
