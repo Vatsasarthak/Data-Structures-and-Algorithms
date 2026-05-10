9a72486968e962518aa54e1af3e842383a32bd48
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-05-11 02:51:05 +0530

    Time: 17 ms (77.85%) | Memory: 45.4 MB (66.16%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,56 +1,26 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int n = nums.length;
         Arrays.sort(nums);
+        int n = nums.length;
+        int clsSum = nums[0]+nums[1]+nums[2];
+        for(int i=0;i<n;i++){
+            int j=i+1, k=n-1;
+            
+            while(j<k){
+                int currSum = nums[i]+nums[j]+nums[k];
 
-        int smallest = nums[0] + nums[1] + nums[2];
-        if (smallest >= target || n == 3) {
-            return smallest;
-        }
-
-        int largest = nums[n - 3] + nums[n - 2] + nums[n - 1];
-        if (largest <= target) {
-            return largest;
-        }
-
-        int closest = largest - target < target - smallest ? largest : smallest;
-        int l = 0, r = n - 1;
-
-        while (r > l + 1 && nums[l] + nums[l + 1] + nums[r] > target) {
-            int sum = nums[l] + nums[l + 1] + nums[r];
-            closest = sum - target < Math.abs(target - closest) ? sum : closest;
-            r--;
-        }
-
-        while (l < r - 1) {
-            int sum = closest;
-            while (l < r - 1 && nums[l] + nums[l + 1] + nums[r] < target) {
-                sum = nums[l] + nums[l + 1] + nums[r];
-                l++;
-            }
-
-            closest = Math.abs(target - sum) < Math.abs(target - closest) ? sum : closest;
-            int mid = l + 1;
-
-            while (l < r - 1 && (0 <= l || mid <= r - 1)) {
-                sum = nums[l] + nums[mid] + nums[r];
-                closest = Math.abs(target - sum) < Math.abs(target - closest) ? sum : closest;
-
-                if (sum > target) {
-                    if (l == 0) break;
-                    l--;
-                } else if (sum < target) {
-                    if (mid == r - 1) break;
-                    mid++;
-                } else {
-                    return closest;
+                if(Math.abs(currSum-target)<Math.abs(clsSum-target)){
+                    clsSum=currSum;
+                }
+                if(currSum<target){
+                    j++;
+                }else if(currSum>target){
+                    k--;
+                }else{
+                    return currSum;
                 }
             }
-
-            r--;
-            l = 0;
         }
-
-        return closest;
+        return clsSum;
     }
 }
