4e0bd507bf8d74f306844d0ec3cec8bb7a47104d
Sarthak vatsa <162605558+Vatsasarthak@users.noreply.github.com>
2026-05-11 05:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (82.33%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,61 +1,50 @@
-import java.util.*;
-
 class Solution {
     public String reorganizeString(String s) {
-        
-        // Step 1: Frequency count
-        int[] freq = new int[26];
         int n = s.length();
-        
-        for(char c : s.toCharArray()){
-            freq[c - 'a']++;
-            
-            // If impossible
-            if(freq[c - 'a'] > (n + 1) / 2){
-                return "";
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        int maxFreq = 0;
+        int letter = 0;
+
+         for (int i = 0; i < 26; i++) {
+            if (freq[i] > maxFreq) {
+                maxFreq = freq[i];
+                letter = i;
             }
         }
 
-        // Step 2: Max Heap (frequency, character)
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> b[0] - a[0]   // max heap by frequency
-        );
-
-        for(int i = 0; i < 26; i++){
-            if(freq[i] > 0){
-                pq.add(new int[]{freq[i], i}); // store index instead of char
-            }
+        if (maxFreq > (n + 1) / 2) {
+            return "";
         }
 
-        StringBuilder res = new StringBuilder();
+        char[] result = new char[n];
 
-        // Step 3: Build result
-        while(!pq.isEmpty()){
-            
-            int[] temp1 = pq.poll();
+        int index = 0;
 
-            // Conflict case
-            if(res.length() > 0 && res.charAt(res.length() - 1) == (char)(temp1[1] + 'a')){
-                
-                int[] temp2 = pq.poll();
+        while (freq[letter] > 0) {
+            result[index] = (char)(letter + 'a');
+            index += 2;
+            freq[letter]--;
+        }
 
-                res.append((char)(temp2[1] + 'a'));
+         for (int i = 0; i < 26; i++) {
 
-                if(temp2[0] - 1 > 0){
-                    pq.add(new int[]{temp2[0] - 1, temp2[1]});
+            while (freq[i] > 0) {
+
+                if (index >= n) {
+                    index = 1;
                 }
 
-                pq.add(temp1);
-            }
-            else{
-                res.append((char)(temp1[1] + 'a'));
-
-                if(temp1[0] - 1 > 0){
-                    pq.add(new int[]{temp1[0] - 1, temp1[1]});
-                }
+                result[index] = (char)(i + 'a');
+                index += 2;
+                freq[i]--;
             }
         }
 
-        return res.toString();
+        return new String(result);
     }
 }
